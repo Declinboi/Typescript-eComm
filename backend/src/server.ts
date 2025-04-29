5f3c427e80d77883edc73237470a39e930d64aa9
declinboi <jacobclinton2234@gmail.com>
2025-04-29 12:18:54 +0100

    cors origin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
-app.use(cors());
+app.use(cors({
+  origin: 'https://typescript-e-comm-iqtx.vercel.app',
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
