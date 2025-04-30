e588e8a900949e6ebd0d073266145208cc5ced75
declinboi <jacobclinton2234@gmail.com>
2025-04-30 16:06:29 +0100

    toop

--- frontend/src/pages/Products/FavoriteCount.tsx
+++ frontend/src/pages/Products/FavoriteCount.tsx
@@ -6,7 +6,7 @@ const FavoritesCount = () => {
   const favoriteCount = favorites.length;
 
   return (
-    <div className="absolute sm:left-2 sm:top-8 left-0 top-0 ">
+    <div className="absolute sm:left-2 sm:top-8 left-0 top-2 ">
       {favoriteCount > 0 && (
         <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
           {favoriteCount}
