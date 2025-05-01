a669becc72a02e5df074a3dd20d114fac9211b77
declinboi <jacobclinton2234@gmail.com>
2025-05-01 20:30:48 +0100

    margin

--- frontend/src/pages/Orders/OrderPage.tsx
+++ frontend/src/pages/Orders/OrderPage.tsx
@@ -257,143 +257,141 @@ const OrderPage = () => {
     //   </div>
     // </div>
 
-    <div className="container ml-[10rem] flex flex-col md:flex-row px-4">
-  <div className="md:w-2/3 md:pr-4">
-    <div className="border border-gray-300 mt-5 pb-4 mb-5">
-      {order.orderItems.length === 0 ? (
-        <Message>Order is empty</Message>
-      ) : (
-        // Wrapper for horizontal scroll
-        <div className="w-full overflow-x-auto">
-          <div className="flex justify-end min-w-[800px]">
-            <table className="w-full max-w-5xl">
-              <thead className="border-b-2">
-                <tr>
-                  <th className="p-2">Image</th>
-                  <th className="p-2">Product</th>
-                  <th className="p-2 text-center">Quantity</th>
-                  <th className="p-2">Unit Price</th>
-                  <th className="p-2">Total</th>
-                </tr>
-              </thead>
+    <div className="container ml-[5rem] flex flex-col md:flex-row px-4">
+      <div className="md:w-2/3 md:pr-4">
+        <div className="border border-gray-300 mt-5 pb-4 mb-5">
+          {order.orderItems.length === 0 ? (
+            <Message>Order is empty</Message>
+          ) : (
+            // Wrapper for horizontal scroll
+            <div className="w-full overflow-x-auto">
+              <div className="flex justify-end min-w-[800px]">
+                <table className="w-full max-w-5xl">
+                  <thead className="border-b-2">
+                    <tr>
+                      <th className="p-2">Image</th>
+                      <th className="p-2">Product</th>
+                      <th className="p-2 text-center">Quantity</th>
+                      <th className="p-2">Unit Price</th>
+                      <th className="p-2">Total</th>
+                    </tr>
+                  </thead>
 
-              <tbody>
-                {order.orderItems.map((item: any, index: number) => (
-                  <tr key={index}>
-                    <td className="p-2">
-                      <img
-                        src={item.image}
-                        alt={item.name}
-                        className="w-16 h-16 rounded-lg object-cover"
-                      />
-                    </td>
-                    <td className="p-2">
-                      <Link to={`/product-details/${item.product}`}>
-                        {item.name}
-                      </Link>
-                    </td>
-                    <td className="p-2 text-center">{item.qty}</td>
-                    <td className="p-2 text-center">{item.price}</td>
-                    <td className="p-2 text-center">
-                      $ {(item.qty * item.price).toFixed(2)}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                  <tbody>
+                    {order.orderItems.map((item: any, index: number) => (
+                      <tr key={index}>
+                        <td className="p-2">
+                          <img
+                            src={item.image}
+                            alt={item.name}
+                            className="w-16 h-16 rounded-lg object-cover"
+                          />
+                        </td>
+                        <td className="p-2">
+                          <Link to={`/product-details/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </td>
+                        <td className="p-2 text-center">{item.qty}</td>
+                        <td className="p-2 text-center">{item.price}</td>
+                        <td className="p-2 text-center">
+                          $ {(item.qty * item.price).toFixed(2)}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  </div>
+      </div>
 
-  <div className="md:w-1/3">
-    <div className="mt-5 p-2 rounded-lg border-dotted border-2 border-green-300 pb-4 mb-4">
-      <h2 className="text-xl font-bold mb-2">Shipping</h2>
-      <p className="mb-4 mt-4">
-        <strong className="text-green-500">Order:</strong> {order._id}
-      </p>
-      <p className="mb-4">
-        <strong className="text-green-500">Name:</strong> {order.user.username}
-      </p>
-      <p className="mb-4">
-        <strong className="text-green-500">Email:</strong> {order.user.email}
-      </p>
-      <p className="mb-4">
-        <strong className="text-green-500">Address:</strong>{" "}
-        {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
-        {order.shippingAddress.postalCode}, {order.shippingAddress.country}
-      </p>
-      <p className="mb-4">
-        <strong className="text-green-500">Method:</strong>{" "}
-        {order.paymentMethod}
-      </p>
+      <div className="md:w-1/3">
+        <div className="mt-5 p-2 rounded-lg border-dotted border-2 border-green-300 pb-4 mb-4">
+          <h2 className="text-xl font-bold mb-2">Shipping</h2>
+          <p className="mb-4 mt-4">
+            <strong className="text-green-500">Order:</strong> {order._id}
+          </p>
+          <p className="mb-4">
+            <strong className="text-green-500">Name:</strong>{" "}
+            {order.user.username}
+          </p>
+          <p className="mb-4">
+            <strong className="text-green-500">Email:</strong>{" "}
+            {order.user.email}
+          </p>
+          <p className="mb-4">
+            <strong className="text-green-500">Address:</strong>{" "}
+            {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
+            {order.shippingAddress.postalCode}, {order.shippingAddress.country}
+          </p>
+          <p className="mb-4">
+            <strong className="text-green-500">Method:</strong>{" "}
+            {order.paymentMethod}
+          </p>
 
-      {order.isPaid ? (
-        <Message variant="success">Paid on {order.paidAt}</Message>
-      ) : (
-        <Message variant="error">Not paid</Message>
-      )}
-    </div>
+          {order.isPaid ? (
+            <Message variant="success">Paid on {order.paidAt}</Message>
+          ) : (
+            <Message variant="error">Not paid</Message>
+          )}
+        </div>
 
-    <h2 className="text-xl font-bold mb-2 mt-[3rem]">Order Summary</h2>
-    <div className="flex justify-between mb-2">
-      <span>Items</span>
-      <span>$ {order.itemsPrice}</span>
-    </div>
-    <div className="flex justify-between mb-2">
-      <span>Shipping</span>
-      <span>$ {order.shippingPrice}</span>
-    </div>
-    <div className="flex justify-between mb-2">
-      <span>Tax</span>
-      <span>$ {order.taxPrice}</span>
-    </div>
-    <div className="flex justify-between mb-2">
-      <span>Total</span>
-      <span>$ {order.totalPrice}</span>
-    </div>
+        <h2 className="text-xl font-bold mb-2 mt-[3rem]">Order Summary</h2>
+        <div className="flex justify-between mb-2">
+          <span>Items</span>
+          <span>$ {order.itemsPrice}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span>Shipping</span>
+          <span>$ {order.shippingPrice}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span>Tax</span>
+          <span>$ {order.taxPrice}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span>Total</span>
+          <span>$ {order.totalPrice}</span>
+        </div>
 
-    {!order.isPaid && (
-      <div>
-        {loadingPay && (
+        {!order.isPaid && (
+          <div>
+            {loadingPay && (
+              <Loader className="h-4 w-4 animate-spin text-emerald-800" />
+            )}
+            {isPending ? (
+              <Loader className="h-4 w-4 animate-spin text-gray-800" />
+            ) : (
+              <div>
+                <PayPalButtons
+                  key={orderId}
+                  createOrder={createOrder}
+                  onApprove={onApprove}
+                  onError={onError}
+                />
+              </div>
+            )}
+          </div>
+        )}
+
+        {loadingDeliver && (
           <Loader className="h-4 w-4 animate-spin text-emerald-800" />
         )}
-        {isPending ? (
-          <Loader className="h-4 w-4 animate-spin text-gray-800" />
-        ) : (
+        {userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
           <div>
-            <PayPalButtons
-              key={orderId}
-              createOrder={createOrder}
-              onApprove={onApprove}
-              onError={onError}
-            />
+            <button
+              type="button"
+              className="bg-green-500 text-white w-full py-2"
+              onClick={deliverHandler}
+            >
+              Mark As Delivered
+            </button>
           </div>
         )}
       </div>
-    )}
-
-    {loadingDeliver && (
-      <Loader className="h-4 w-4 animate-spin text-emerald-800" />
-    )}
-    {userInfo &&
-      userInfo.isAdmin &&
-      order.isPaid &&
-      !order.isDelivered && (
-        <div>
-          <button
-            type="button"
-            className="bg-green-500 text-white w-full py-2"
-            onClick={deliverHandler}
-          >
-            Mark As Delivered
-          </button>
-        </div>
-      )}
-  </div>
-</div>
-
+    </div>
   );
 };
 
