a5f790637f723e38b6f929f9192677ddc49b17f1
declinboi <jacobclinton2234@gmail.com>
2025-05-02 14:34:31 +0100

    joint width

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,42 +13,40 @@ const Home = () => {
 
   return (
     <>
-      <div className="">
-        {!keyword ? <Header /> : null}
-        {isLoading ? (
-          <Loader className="h-8 w-8 animate-spin text-emerald-800" />
-        ) : error ? (
-          <Message variant="error">
-            {(error as { data?: { message?: string }; error?: string })?.data
-              ?.message || (error as { error?: string })?.error}
-          </Message>
-        ) : (
-          <>
-            <div className="flex flex-col md:flex-row justify-between items-center px-4 mt-20 md:mt-40">
-              <h1 className="text-3xl text-center md:text-5xl font-bold mb-4 md:mb-0 md:ml-20">
-                Special Products
-              </h1>
+      {!keyword ? <Header /> : null}
+      {isLoading ? (
+        <Loader className="h-8 w-8 animate-spin text-emerald-800" />
+      ) : error ? (
+        <Message variant="error">
+          {(error as { data?: { message?: string }; error?: string })?.data
+            ?.message || (error as { error?: string })?.error}
+        </Message>
+      ) : (
+        <>
+          <div className="flex flex-col md:flex-row justify-between items-center px-4 mt-20 md:mt-40">
+            <h1 className="text-3xl text-center md:text-5xl font-bold mb-4 md:mb-0 md:ml-20">
+              Special Products
+            </h1>
 
-              <Link
-                to="/shop"
-                className="bg-green-600 font-bold shadow-lg rounded-full py-2 px-10 md:mr-20"
-              >
-                Shop
-              </Link>
-            </div>
+            <Link
+              to="/shop"
+              className="bg-green-600 font-bold shadow-lg rounded-full py-2 px-10 md:mr-20"
+            >
+              Shop
+            </Link>
+          </div>
 
-            <div>
-              <div className="flex justify-center flex-wrap mt-8 rounded-lg shadow-lg gap-4">
-                {data.products.map((product: Product) => (
-                  <div key={product._id}>
-                    <ProductH product={product} />
-                  </div>
-                ))}
-              </div>
+          <div>
+            <div className="flex justify-center flex-wrap mt-8 rounded-lg shadow-lg gap-4">
+              {data.products.map((product: Product) => (
+                <div key={product._id}>
+                  <ProductH product={product} />
+                </div>
+              ))}
             </div>
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
